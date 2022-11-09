d1a445b43b376627aaddaddcc474b026411074b1
Edward Griffin <100326692+edwardmgriffin95@users.noreply.github.com>
2022-11-09 13:53:45 -0500

    Create length-of-last-word.js

---
+++ length-of-last-word.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let trimmedString = s.trim();
+
+    return trimmedString.length - trimmedString.lastIndexOf(' ') - 1;
+    
+};
